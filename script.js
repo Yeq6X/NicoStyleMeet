18bb6a372f73e96d4a6a7f46993c02e81d5c17ce
kawamataryou <ryo.kawamata@lapras.com>
2021-03-25 11:50:24 +0900

    [from now] 2021/03/25 11:50:24
    diff --git a/script.js b/script.js
    index 8545f49..dfc6a12 100644
    --- a/script.js
    +++ b/script.js
    @@ -2,37 +2,48 @@ var prevThread;
    
     var observer = new MutationObserver(records=>{
     try {
    +  // テキスト欄の取得
       const thread = document.getElementsByClassName('z38b6 CnDs7d hPqowe')[0];
    -
    +  // テキスト欄以外のDOM変化で発火したら無視
       if (prevThread != undefined && thread.isEqualNode(prevThread)) return;
    +  // ????
       if (thread.getElementsByClassName('gYckH').length == 1) return;
    +  // 比較用にグローバルに格納
       prevThread = thread.cloneNode(true);
    +  // メッセージそれぞれを配列として取得
       const messages = thread.getElementsByClassName('oIy2qc');
    +  // 最後のメッセージを取得
       const message = messages[messages.length-1].innerText;
    
       var screen = document.body; // よくない
       var screenHeight = screen.offsetHeight;
       var screenWidth = screen.offsetWidth;
    
    +  // コメントのエレメント作成
       var comment = document.createElement('span');
    
    +  // コメントへのメッセージの追加
       comment.textContent = message;
    +  // コメントのbodyへの追加
       document.getElementsByTagName('body')[0].appendChild(comment);
    
       var letterSize = screenHeight*0.05;
    
       comment.setAttribute('class', 'comment');
    
    +  // コメントのスタイル作成
       var commentStyle = {
         left: screenWidth + 'px',
         top: Math.floor((screenHeight - letterSize) * Math.random()) + 'px',
         fontSize: letterSize + 'px',
       }
    +  // スタイル設定
       for(var prop in commentStyle) {
         comment.style[prop] = commentStyle[prop];
       }
    
    +  // アニメーション
       $(comment).animate(
         {
           'left': -comment.offsetWidth + 'px'
    @@ -58,15 +69,7 @@ const config = {
     }
    
     document.addEventListener('DOMContentLoaded', () => {
    +  // オブザーバー
       var elem = document.body; // ほんとはチャットボックスを監視するべきだけどmeetに参加画面と判断するのめんどくさい これのせいで重い
       observer.observe(elem, config);
    -
    -  var cas = document.createElement('iframe');
    -  cas.setAttribute('src', 'https://river.tango-gacha.com/')
    -  cas.setAttribute('width', '100%')
    -  cas.setAttribute('height', '100%')
    -  cas.setAttribute('frameborder', '0')
    -  cas.setAttribute('style', "position:absolute;border:0;width:100%;filter:invert(100%);-webkit-filter:invert(100%);z-index:2147483647;pointer-events:none;")
    -  var elem = document.body;
    -  elem.appendChild(cas)
     });

--- script.js
+++ script.js
@@ -2,37 +2,48 @@ var prevThread;
 
 var observer = new MutationObserver(records=>{
 try {
+  // テキスト欄の取得
   const thread = document.getElementsByClassName('z38b6 CnDs7d hPqowe')[0];
-
+  // テキスト欄以外のDOM変化で発火したら無視
   if (prevThread != undefined && thread.isEqualNode(prevThread)) return;
+  // ????
   if (thread.getElementsByClassName('gYckH').length == 1) return;
+  // 比較用にグローバルに格納
   prevThread = thread.cloneNode(true);
+  // メッセージそれぞれを配列として取得
   const messages = thread.getElementsByClassName('oIy2qc');
+  // 最後のメッセージを取得
   const message = messages[messages.length-1].innerText;
 
   var screen = document.body; // よくない
   var screenHeight = screen.offsetHeight;
   var screenWidth = screen.offsetWidth;
 
+  // コメントのエレメント作成
   var comment = document.createElement('span');
 
+  // コメントへのメッセージの追加
   comment.textContent = message;
+  // コメントのbodyへの追加
   document.getElementsByTagName('body')[0].appendChild(comment);
 
   var letterSize = screenHeight*0.05;
 
   comment.setAttribute('class', 'comment');
 
+  // コメントのスタイル作成
   var commentStyle = {
     left: screenWidth + 'px',
     top: Math.floor((screenHeight - letterSize) * Math.random()) + 'px',
     fontSize: letterSize + 'px',
   }
+  // スタイル設定
   for(var prop in commentStyle) {
     comment.style[prop] = commentStyle[prop];
   }
 
 
+  // アニメーション
   $(comment).animate(
     {
       'left': -comment.offsetWidth + 'px'
@@ -58,15 +69,7 @@ const config = {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+  // オブザーバー
   var elem = document.body; // ほんとはチャットボックスを監視するべきだけどmeetに参加画面と判断するのめんどくさい これのせいで重い
   observer.observe(elem, config);
-
-  var cas = document.createElement('iframe');
-  cas.setAttribute('src', 'https://river.tango-gacha.com/')
-  cas.setAttribute('width', '100%')
-  cas.setAttribute('height', '100%')
-  cas.setAttribute('frameborder', '0')
-  cas.setAttribute('style', "position:absolute;border:0;width:100%;filter:invert(100%);-webkit-filter:invert(100%);z-index:2147483647;pointer-events:none;")
-  var elem = document.body;
-  elem.appendChild(cas)
 });
