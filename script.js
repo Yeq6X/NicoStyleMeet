5023a070d40ec558eff995c81c9a1334be50f503
showwin <showwin_kmc@yahoo.co.jp>
2021-03-25 14:15:13 +0900

    [add] 8888 弾幕

--- script.js
+++ script.js
@@ -44,6 +44,71 @@ function showComment(message) {
   );
 }
 
+function showBarrageClapping(message) {
+  var screen = document.body; // よくない
+  var screenHeight = screen.offsetHeight;
+  var screenWidth = screen.offsetWidth;
+
+  // 5~10個をランダムで複製する
+  var min = 5;
+  var max = 10;
+  var clappingCount = Math.floor( Math.random() * (max + 1 - min) ) + min;
+
+  for (let i = 0; i < clappingCount; i++) {
+    // コメントのエレメント作成
+    var comment = document.createElement('span');
+
+    // コメントへのメッセージの追加
+    comment.textContent = message;
+    // コメントのbodyへの追加
+    document.getElementsByTagName('body')[0].appendChild(comment);
+
+    var letterSize = screenHeight*0.05;
+
+    comment.setAttribute('class', 'comment');
+
+    // コメントのスタイル作成
+    var commentStyle = {
+      left: screenWidth + 'px',
+      top: Math.floor((screenHeight - letterSize) * Math.random()) + 'px',
+      fontSize: letterSize + 'px',
+    }
+    // スタイル設定
+    for(var prop in commentStyle) {
+      comment.style[prop] = commentStyle[prop];
+    }
+
+    // ランダムな速度で複製する
+    var min = 4000;
+    var max = 6000;
+    var duration = Math.floor( Math.random() * (max + 1 - min) ) + min;
+
+    // ランダムな開始位置で複製する
+    var min = 0;
+    var max = 100;  
+    var randomOffset = Math.floor( Math.random() * (max + 1 - min) ) + min;
+
+    // アニメーション
+    $(comment).animate(
+      {
+        'left': - comment.offsetWidth - randomOffset + 'px'
+      },
+      {
+        'duration': duration,
+        'easing': 'linear',
+        'complete': function() {
+          document.getElementsByTagName('body')[0].removeChild(comment);
+        }
+      }
+    );
+  }
+}
+
+function isClapping(message) {
+  const pattern = /^[8８]+$/g;
+  return message.match(pattern);
+}
+
 var observer = new MutationObserver(records=>{
 try {
   // テキスト欄の取得
@@ -59,7 +124,11 @@ try {
   // 最後のメッセージを取得
   const message = messages[messages.length-1].innerText;
 
-  showComment(message)
+  if (isClapping(message)) {
+    showBarrageClapping(message)
+  } else {
+    showComment(message)
+  }
 }
 catch(e) {
   return;
